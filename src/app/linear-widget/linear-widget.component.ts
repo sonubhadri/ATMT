1e5f9ba4c45155787c598fead9a2b18bdcced7eb
Praveen Bhadri <praveen.bhadri@gmail.com>
2018-08-13 16:10:39 +0530

    removing private keyword from the variable initialiazation

--- src/app/linear-widget/linear-widget.component.ts
+++ src/app/linear-widget/linear-widget.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit, Input } from '@angular/core';
 export class LinearWidgetComponent implements OnInit {
 
   @Input() linearCard: any;
-  private sourcetext;
-  private targettext;
-  private englishword;
-  private englishSourceArray = new Array();
+   sourcetext;
+   targettext;
+   englishword;
+   englishSourceArray = new Array();
 
   constructor() { }
 
