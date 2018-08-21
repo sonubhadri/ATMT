e26416985afe9cf1dd33ac37579a3b30fc93d489
Praveen Bhadri <praveen.bhadri@gmail.com>
2018-08-22 01:08:28 +0530

    changes made to show the aligned Strongs and Hindi words

--- src/app/csv-to-table/csv-to-table.component.ts
+++ src/app/csv-to-table/csv-to-table.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Http , Response } from '@angular/http';
-import {GlobalUrl} from '../globalUrl';
+import { Http, Response } from '@angular/http';
+import { GlobalUrl } from '../globalUrl';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
 import 'hammerjs';
 
-export interface PeriodicElement {  
+export interface PeriodicElement {
   position: any;
   English: any;
   Hindi: any;
   Definition: any;
   StrongNumber: any;
   References: any;
-  API:any;
+  AlignedStrongs: any;
+  HindiAlignedWords: any;
 }
 
 @Component({
@@ -21,11 +22,11 @@ export interface PeriodicElement {
 })
 export class CsvToTableComponent implements OnInit {
 
-   ELEMENT_DATA: PeriodicElement[] = [];
-   csvData:any;
-   transData:any;
+  ELEMENT_DATA: PeriodicElement[] = [];
+  csvData: any;
+  transData: any;
 
-  displayedColumns: string[] = ['S.No', 'English Word forms', 'Hindi Translation', 'Definition/Facts/Description', 'Strong Number', 'References', 'API'];
+  displayedColumns: string[] = ['S.No', 'English Word forms', 'Hindi Translation', 'Definition/Facts/Description', 'Strong Number', 'References', 'AlignedStrongs', 'HindiAlignedWords'];
   dataSource = new MatTableDataSource<PeriodicElement>(this.ELEMENT_DATA);
   // @ViewChild(MatPaginator) paginator: MatPaginator;
   // @ViewChild(MatSort) sort: MatSort;
@@ -36,47 +37,56 @@ export class CsvToTableComponent implements OnInit {
     this.getData();
   }
 
-  constructor(private _http:Http,private ApiUrl:GlobalUrl) { }
+  constructor(private _http: Http, private ApiUrl: GlobalUrl) { }
 
-   display = false;
-   showAll(id){
-     if(document.getElementById(id).innerHTML == this.ELEMENT_DATA[id.split('-')[1] - 1].Definition){
-      document.getElementById(id).innerHTML = this.ELEMENT_DATA[id.split('-')[1] - 1].Definition.substring(0,150) + "......";
-     }
-     else
-     document.getElementById(id).innerHTML = this.ELEMENT_DATA[id.split('-')[1] - 1].Definition;
-   }
+  display = false;
+  showAll(id) {
+    if (document.getElementById(id).innerHTML == this.ELEMENT_DATA[id.split('-')[1] - 1].Definition) {
+      document.getElementById(id).innerHTML = this.ELEMENT_DATA[id.split('-')[1] - 1].Definition.substring(0, 150) + "......";
+    }
+    else
+      document.getElementById(id).innerHTML = this.ELEMENT_DATA[id.split('-')[1] - 1].Definition;
+  }
 
-  getData(){
+  getData() {
     this.display = true;
     this._http.get(this.ApiUrl.csvFile)
-    .subscribe(data => {
-       this.csvData = data.text().split(/\r?\n|\r/);
-      for (let i = 1; i < this.csvData.length - 1; i++) {
+      .subscribe(data => {
+        this.csvData = data.text().split(/\r?\n|\r/);
+        for (let i = 1; i < this.csvData.length - 1; i++) {
 
-          
-          let transKey:any = "";
-          if (Object.values(this.transData)[i] != undefined && Object.values(this.transData)[i] != null && Object.values(Object.values(this.transData)[i]) != undefined && Object.values(Object.values(this.transData)[i]) != null){
-              transKey = Object.values(Object.values(this.transData)[i]);
-              //console.log(Object.values(Object.values(this.transData)[i]));
-          }
           let value = this.csvData[i].split('	');
-          this.ELEMENT_DATA.push({ position: value[0], English: value[1], Hindi: value[2], Definition: value[3], StrongNumber:value[4], References:value[5], API:transKey })
+          let transKey: any = "";
+          let hindiAligned: any = "";
+          let strongAligned: any = "";
+          if (Object.values(this.transData)[i - 1] != undefined && Object.values(this.transData)[i - 1] != null && Object.values(Object.values(this.transData)[i - 1]) != undefined && Object.values(Object.values(this.transData)[i - 1]) != null) {
+            transKey = Object.values(Object.values(this.transData)[i - 1]);
 
-        // console.log(value[0]);
-      }
-      // this.dataSource.paginator = this.paginator;
-      // this.dataSource.sort = this.sort;
-      this.display = false;
-    },(error:Response) =>{
-      if(error.status === 404){ 
+            for (let y = 0; y < transKey.length; y++) {
+              if (transKey[y].toString().indexOf(',') > -1) {
+                hindiAligned =  hindiAligned + "," + (transKey[y].toString().split(',')[1].replace(/\)/g, "")).replace(/\'/g, "").replace(/\./g, "");
+
+                strongAligned = strongAligned + "," + (transKey[y].toString().split(',')[0].replace(/\(/g, "")).replace(/\'/g, "").replace(/\./g, "") + "  ";
+              }
+            }
+          }
+          
+          this.ELEMENT_DATA.push({ position: value[0], English: value[1], Hindi: value[2], Definition: value[3], StrongNumber: value[4], References: value[5], AlignedStrongs: (strongAligned == '') ? 'NA' : strongAligned.replace(/\)/g, "").replace(/\./g, ""), HindiAlignedWords: (hindiAligned == '') ? 'NA' : hindiAligned.replace(/\)/g, "").replace(/\./g, "") })
+
+          // console.log(value[0]);
+        }
+        // this.dataSource.paginator = this.paginator;
+        // this.dataSource.sort = this.sort;
         this.display = false;
-      }
-      else{
-        this.display = false;
-      }
-      
-    })
+      }, (error: Response) => {
+        if (error.status === 404) {
+          this.display = false;
+        }
+        else {
+          this.display = false;
+        }
+
+      })
   }
 
   applyFilter(filterValue: string) {
@@ -88,26 +98,26 @@ export class CsvToTableComponent implements OnInit {
   }
 
 
-  getSampeTranslationData(){
+  getSampeTranslationData() {
     this._http.get(this.ApiUrl.sampleFile)
-    .subscribe(data => {
-      this.transData = data.json();
-      // console.log(data.json())
-      // console.log(Object.keys(data.json()).length);
-      for(let i =1; i <= Object.keys(data.json()).length; i++){
-        //console.log(data.json()[i]);     
-      }
-       
-      
-    },(error:Response) =>{
-      if(error.status === 404){
-        this.display = false;
-      }
-      else{
-        this.display = false;
-      }
-      
-    })
+      .subscribe(data => {
+        this.transData = data.json();
+        // console.log(data.json())
+        // console.log(Object.keys(data.json()).length);
+        // for(let i =1; i <= Object.keys(data.json()).length; i++){
+        //   //console.log(data.json()[i]);     
+        // }
+
+
+      }, (error: Response) => {
+        if (error.status === 404) {
+          this.display = false;
+        }
+        else {
+          this.display = false;
+        }
+
+      })
   }
 
 }
