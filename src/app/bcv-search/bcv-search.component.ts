d645c1a0b73fd6b3a544414aea52b487f599dd50
Praveen Bhadri <praveen.bhadri@gmail.com>
2018-12-03 14:27:06 +0530

    Changes made for greek manual mapping

--- src/app/bcv-search/bcv-search.component.ts
+++ src/app/bcv-search/bcv-search.component.ts
@@ -33,6 +33,7 @@ export class BcvSearchComponent implements OnInit {
   LangArray: any; //= new Array();
   langParam: any;
   NextFlag: boolean = false;
+  greekBcvArray: any;
 
   constructor(public router: Router, private activatedRoute: ActivatedRoute, private toastr: ToastrService, private _http: Http, private ApiUrl: GlobalUrl) {
 
@@ -155,22 +156,74 @@ export class BcvSearchComponent implements OnInit {
       this.verseNumber = stringify(0);
       this.BCV = null
       this.langParam = l;
+      console.log(l)
 
       localStorage.setItem('language', this.langParam);
 
-      this._http.get(this.ApiUrl.getBooks + '/' + l + '/grk-ugnt')
-        .subscribe(data => {
-          this.Books = data.json().books;
-          //console.log (data.json())
-        }, (error: Response) => {
-          if (error.status === 404) {
-            this.toastr.warning("Books data not available")
-          }
-          else {
-            this.toastr.error("An Unexpected Error Occured.")
-          }
+      if (l == 'grk-UGNT4') {
 
-        })
+        enum booknoo {
+          "GEN" = "01", "EXO" = "02", "LEV" = "03", "NUM" = "04", "DEU" = "05",
+          "JOS" = "06", "JDG" = "07", "RUT" = "08", '1SA' = "09", "2SA" = 10, "1KI" = 11, "2KI" = 12,
+          "1CH" = 13, "2CH" = 14, "EZR" = 15, "NEH" = 16, "EST" = 17, "JOB" = 18, "PSA" = 19
+          , "PRO" = 20, "ECC" = 21, "SNG" = 22, "ISA" = 23, "JER" = 24, "LAM" = 25, "EZK" = 26,
+          "DAN" = 27, "HOS" = 28, "JOL" = 29, "AMO" = 30, "OBA" = 31, "JON" = 32, "MIC" = 33,
+          "NAM" = 34, "HAB" = 35, "ZEP" = 36, "HAG" = 37, "ZEC" = 38, "MAL" = 39, "MAT" = 40,
+          "MRK" = 41, "LUK" = 42, "JHN" = 43, "ACT" = 44, "ROM" = 45, "1CO" = 46, "2CO" = 47,
+          "GAL" = 48, "EPH" = 49, "PHP" = 50, "COL" = 51, "1TH" = 52, "2TH" = 53, "1TI" = 54,
+          "2TI" = 55, "TIT" = 56, "PHM" = 57, "HEB" = 58, "JAS" = 59, "1PE" = 60, "2PE" = 61,
+          "1JN" = 62, "2JN" = 63, "3JN" = 64, "JUD" = 65, "REV" = 66
+        };
+
+        this._http.get('https://autographa.herokuapp.com/assets/ugnt4_ourGreek_Mapping_VERSE_navigation.json')
+          .subscribe(data => {
+            let greekBCVArray = new Array();
+            let firstBook = new Array();
+            let firstchap = new Array();
+            let firstVerse = new Array();
+            greekBCVArray = data.json();
+            this.greekBcvArray = greekBCVArray;
+            firstBook.push(booknoo[greekBCVArray[0].substring(0, 2)]);
+            this.bookFirstIndex = firstBook[0];
+            this.Books = firstBook;
+
+
+            let chapterstr = greekBCVArray[0].substring(2, 5).replace(/^0+/, '');
+            firstchap.push(chapterstr);
+            this.chapterFirstIndex = firstchap[0];
+            this.Chapters = firstchap;
+
+
+            let versestr = greekBCVArray[0].substring(5, 8).replace(/^0+/, '');
+            firstVerse.push(versestr)
+            this.verseFirstIndex = versestr[0];
+            this.Verses = firstVerse;
+
+            this.bookNumber = greekBCVArray[0].substring(0, 2);
+            this.chapterNumber = greekBCVArray[0].substring(2, 5);
+
+            this.verseChange(versestr);
+            // console.log(firstBook)
+            // console.log(data.json())
+          })
+
+      }
+      else {
+
+        this._http.get(this.ApiUrl.getBooks + '/' + l + '/grk-ugnt')
+          .subscribe(data => {
+            this.Books = data.json().books;
+            //console.log (data.json())
+          }, (error: Response) => {
+            if (error.status === 404) {
+              this.toastr.warning("Books data not available")
+            }
+            else {
+              this.toastr.error("An Unexpected Error Occured.")
+            }
+
+          })
+      }
     }
   }
 
@@ -273,31 +326,189 @@ export class BcvSearchComponent implements OnInit {
   }
 
   prevOnclick() {
-    (<HTMLInputElement>document.getElementById("nxtbtn")).disabled = true;
-    (<HTMLInputElement>document.getElementById("prebtn")).disabled = true;
-    document.getElementById('grid').scrollTop = 0;
-    if (document.getElementById("saveButton").style.display != "none") {
-      this.toastr.warning("Kindly click on save to make the updation or discard changes.")
+
+    if (localStorage.getItem('language') == 'grk-UGNT4') {
+      (<HTMLInputElement>document.getElementById("nxtbtn")).disabled = true;
+      (<HTMLInputElement>document.getElementById("prebtn")).disabled = true;
+
+      enum booknoo {
+        "GEN" = "01", "EXO" = "02", "LEV" = "03", "NUM" = "04", "DEU" = "05",
+        "JOS" = "06", "JDG" = "07", "RUT" = "08", '1SA' = "09", "2SA" = 10, "1KI" = 11, "2KI" = 12,
+        "1CH" = 13, "2CH" = 14, "EZR" = 15, "NEH" = 16, "EST" = 17, "JOB" = 18, "PSA" = 19
+        , "PRO" = 20, "ECC" = 21, "SNG" = 22, "ISA" = 23, "JER" = 24, "LAM" = 25, "EZK" = 26,
+        "DAN" = 27, "HOS" = 28, "JOL" = 29, "AMO" = 30, "OBA" = 31, "JON" = 32, "MIC" = 33,
+        "NAM" = 34, "HAB" = 35, "ZEP" = 36, "HAG" = 37, "ZEC" = 38, "MAL" = 39, "MAT" = 40,
+        "MRK" = 41, "LUK" = 42, "JHN" = 43, "ACT" = 44, "ROM" = 45, "1CO" = 46, "2CO" = 47,
+        "GAL" = 48, "EPH" = 49, "PHP" = 50, "COL" = 51, "1TH" = 52, "2TH" = 53, "1TI" = 54,
+        "2TI" = 55, "TIT" = 56, "PHM" = 57, "HEB" = 58, "JAS" = 59, "1PE" = 60, "2PE" = 61,
+        "1JN" = 62, "2JN" = 63, "3JN" = 64, "JUD" = 65, "REV" = 66
+      };
+
+      let greekBCVArray = new Array();
+      let firstBook = new Array();
+      let firstchap = new Array();
+      let firstVerse = new Array();
+      greekBCVArray = this.greekBcvArray;
+      let indexOfBCV = greekBCVArray.indexOf(this.BCV);
+      //console.log(greekBCVArray[indexOfBCV + 1])
+
+      firstBook.push(booknoo[greekBCVArray[indexOfBCV - 1].substring(0, 2)]);
+      this.bookFirstIndex = firstBook[0];
+      this.Books = firstBook;
+
+
+      let chapterstr = greekBCVArray[indexOfBCV - 1].substring(2, 5).replace(/^0+/, '');
+      firstchap.push(chapterstr);
+      this.chapterFirstIndex = firstchap[0];
+      this.Chapters = firstchap;
+
+
+      let versestr = greekBCVArray[indexOfBCV - 1].substring(5, 8).replace(/^0+/, '');
+      firstVerse.push(versestr)
+
+      this.Verses = firstVerse;
+      this.verseFirstIndex = firstVerse[0];
+
+      this.bookNumber = greekBCVArray[indexOfBCV - 1].substring(0, 2);
+      this.chapterNumber = greekBCVArray[indexOfBCV - 1].substring(2, 5);
+
+      this.verseChange(versestr);
+      // console.log(firstBook)
+      // console.log(data.json())
+
+
+    }
+
+
+    else {
+      (<HTMLInputElement>document.getElementById("nxtbtn")).disabled = true;
+      (<HTMLInputElement>document.getElementById("prebtn")).disabled = true;
+      document.getElementById('grid').scrollTop = 0;
+      if (document.getElementById("saveButton").style.display != "none") {
+        this.toastr.warning("Kindly click on save to make the updation or discard changes.")
+      }
+
+      else {
+
+        if (this.verseNumber != '000') {
+          document.getElementById("saveButton").style.display = "none";
+          document.getElementById("appButton").style.display = 'none';
+          document.getElementById("discardButton").style.display = 'none';
+        }
+
+        if (this.chapterNumber && this.verseNumber) {
+
+          (Number(this.verseNumber) <= this.Verses.length
+            && Number(this.verseNumber) > 1)
+            ? this.verseNumber = stringify((Number(this.verseNumber) - 1))
+            : this.verseNumber
+
+
+          this.verseFirstIndex = Number(this.verseNumber);
+
+
+          let U: string = this.verseNumber;
+          if (U.toString().length > 2) { this.verseNumber = U }
+          else if (U.toString().length > 1) { this.verseNumber = '0' + U }
+          else if (U.toString().length === 1) { this.verseNumber = '00' + U };
+          //console.log(this.verseNumber)
+
+
+          if (this.verseNumber == '000') { this.BCV = null }
+          else
+            //this.BCV = this.bookNumber + this.chapterNumber + this.verseNumber;
+            this.BCV = this.bookNumber + this.chapterNumber + this.verseNumber;
+
+          //console.log('prev' + this.bookNumber + this.chapterNumber + this.verseNumber);
+
+          //console.log (this.BCV + "  " + "prev")
+        }
+      }
+      localStorage.setItem("lastAlignments", "");
+      this.NextFlag = false;
+    }
+  }
+
+  nextOnClick() {
+
+    if (localStorage.getItem('language') == 'grk-UGNT4') {
+      (<HTMLInputElement>document.getElementById("nxtbtn")).disabled = true;
+      (<HTMLInputElement>document.getElementById("prebtn")).disabled = true;
+      enum booknoo {
+        "GEN" = "01", "EXO" = "02", "LEV" = "03", "NUM" = "04", "DEU" = "05",
+        "JOS" = "06", "JDG" = "07", "RUT" = "08", '1SA' = "09", "2SA" = 10, "1KI" = 11, "2KI" = 12,
+        "1CH" = 13, "2CH" = 14, "EZR" = 15, "NEH" = 16, "EST" = 17, "JOB" = 18, "PSA" = 19
+        , "PRO" = 20, "ECC" = 21, "SNG" = 22, "ISA" = 23, "JER" = 24, "LAM" = 25, "EZK" = 26,
+        "DAN" = 27, "HOS" = 28, "JOL" = 29, "AMO" = 30, "OBA" = 31, "JON" = 32, "MIC" = 33,
+        "NAM" = 34, "HAB" = 35, "ZEP" = 36, "HAG" = 37, "ZEC" = 38, "MAL" = 39, "MAT" = 40,
+        "MRK" = 41, "LUK" = 42, "JHN" = 43, "ACT" = 44, "ROM" = 45, "1CO" = 46, "2CO" = 47,
+        "GAL" = 48, "EPH" = 49, "PHP" = 50, "COL" = 51, "1TH" = 52, "2TH" = 53, "1TI" = 54,
+        "2TI" = 55, "TIT" = 56, "PHM" = 57, "HEB" = 58, "JAS" = 59, "1PE" = 60, "2PE" = 61,
+        "1JN" = 62, "2JN" = 63, "3JN" = 64, "JUD" = 65, "REV" = 66
+      };
+
+      let greekBCVArray = new Array();
+      let firstBook = new Array();
+      let firstchap = new Array();
+      let firstVerse = new Array();
+      greekBCVArray = this.greekBcvArray;
+      let indexOfBCV = greekBCVArray.indexOf(this.BCV);
+      //console.log(greekBCVArray[indexOfBCV + 1])
+
+      firstBook.push(booknoo[greekBCVArray[indexOfBCV + 1].substring(0, 2)]);
+      this.bookFirstIndex = firstBook[0];
+      this.Books = firstBook;
+
+
+      let chapterstr = greekBCVArray[indexOfBCV + 1].substring(2, 5).replace(/^0+/, '');
+      firstchap.push(chapterstr);
+      this.chapterFirstIndex = firstchap[0];
+      this.Chapters = firstchap;
+
+
+      let versestr = greekBCVArray[indexOfBCV + 1].substring(5, 8).replace(/^0+/, '');
+      firstVerse.push(versestr)
+
+      this.Verses = firstVerse;
+      this.verseFirstIndex = firstVerse[0];
+
+      this.bookNumber = greekBCVArray[indexOfBCV + 1].substring(0, 2);
+      this.chapterNumber = greekBCVArray[indexOfBCV + 1].substring(2, 5);
+
+      this.verseChange(versestr);
+      // console.log(firstBook)
+      // console.log(data.json())
+
+
     }
 
     else {
+      (<HTMLInputElement>document.getElementById("nxtbtn")).disabled = true;
+      (<HTMLInputElement>document.getElementById("prebtn")).disabled = true;
 
-      if (this.verseNumber != '000') {
-        document.getElementById("saveButton").style.display = "none";
-        document.getElementById("appButton").style.display = 'none';
-        document.getElementById("discardButton").style.display = 'none';
+      document.getElementById('grid').scrollTop = 0;
+      if (document.getElementById("saveButton").style.display != "none") {
+        this.toastr.warning("Kindly click on save to make the updation or discard changes.")
       }
 
-      if (this.chapterNumber && this.verseNumber) {
+      else {
 
-        (Number(this.verseNumber) <= this.Verses.length
-          && Number(this.verseNumber) > 1)
-          ? this.verseNumber = stringify((Number(this.verseNumber) - 1))
+        //console.log(this.bookNumber + this.chapterNumber)
+
+        if (this.verseNumber != '000') {
+
+          document.getElementById("saveButton").style.display = "none";
+          document.getElementById("appButton").style.display = 'none';
+          document.getElementById("discardButton").style.display = 'none';
+        }
+        //console.log(this.chapterNumber);
+
+        (Number(this.verseNumber) < this.Verses.length
+          && Number(this.verseNumber) > 0)
+          ? this.verseNumber = stringify((Number(this.verseNumber) + 1))
           : this.verseNumber
 
-
         this.verseFirstIndex = Number(this.verseNumber);
-
 
         let U: string = this.verseNumber;
         if (U.toString().length > 2) { this.verseNumber = U }
@@ -305,64 +516,16 @@ export class BcvSearchComponent implements OnInit {
         else if (U.toString().length === 1) { this.verseNumber = '00' + U };
         //console.log(this.verseNumber)
 
+        //console.log('prev' + this.bookNumber + this.chapterNumber + this.verseNumber);
 
         if (this.verseNumber == '000') { this.BCV = null }
         else
-          //this.BCV = this.bookNumber + this.chapterNumber + this.verseNumber;
           this.BCV = this.bookNumber + this.chapterNumber + this.verseNumber;
+        //console.log (this.BCV + "  " + "next")
 
-        //console.log('prev' + this.bookNumber + this.chapterNumber + this.verseNumber);
-
-        //console.log (this.BCV + "  " + "prev")
+        localStorage.setItem("lastAlignments", "");
+        this.NextFlag = true;
       }
-    }
-    localStorage.setItem("lastAlignments", "");
-    this.NextFlag = false;
-  }
-
-  nextOnClick() {
-    (<HTMLInputElement>document.getElementById("nxtbtn")).disabled = true;
-    (<HTMLInputElement>document.getElementById("prebtn")).disabled = true;
-
-    document.getElementById('grid').scrollTop = 0;
-    if (document.getElementById("saveButton").style.display != "none") {
-      this.toastr.warning("Kindly click on save to make the updation or discard changes.")
-    }
-
-    else {
-
-      //console.log(this.bookNumber + this.chapterNumber)
-
-      if (this.verseNumber != '000') {
-
-        document.getElementById("saveButton").style.display = "none";
-        document.getElementById("appButton").style.display = 'none';
-        document.getElementById("discardButton").style.display = 'none';
-      }
-      //console.log(this.chapterNumber);
-
-      (Number(this.verseNumber) < this.Verses.length
-        && Number(this.verseNumber) > 0)
-        ? this.verseNumber = stringify((Number(this.verseNumber) + 1))
-        : this.verseNumber
-
-      this.verseFirstIndex = Number(this.verseNumber);
-
-      let U: string = this.verseNumber;
-      if (U.toString().length > 2) { this.verseNumber = U }
-      else if (U.toString().length > 1) { this.verseNumber = '0' + U }
-      else if (U.toString().length === 1) { this.verseNumber = '00' + U };
-      //console.log(this.verseNumber)
-
-      //console.log('prev' + this.bookNumber + this.chapterNumber + this.verseNumber);
-
-      if (this.verseNumber == '000') { this.BCV = null }
-      else
-        this.BCV = this.bookNumber + this.chapterNumber + this.verseNumber;
-      //console.log (this.BCV + "  " + "next")
-
-      localStorage.setItem("lastAlignments", "");
-      this.NextFlag = true;
     }
   }
 
